73d0dd8791fcfb4ab3dde64be2b4100c6aa1b2dd
dokkumeme <129614660+dokkumeme@users.noreply.github.com>
2024-06-17 19:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def health():
     return 'OK', 200
 
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'dokkumemeok'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
