5df683b0c35f292f7649d1b7bdc0dd5f7ccc367e
dokkumeme <129614660+dokkumeme@users.noreply.github.com>
2024-09-15 16:07:38 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -15,41 +15,73 @@ logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
 
 
-@Client.on_callback_query(filters.regex(r'^index'))
-async def index_files(bot, query):
-    if query.data.startswith('index_cancel'):
-        temp.CANCEL = True
-        return await query.answer("Cancelling Indexing")
-    _, raju, chat, lst_msg_id, from_user = query.data.split("#")
-    if raju == 'reject':
-        await query.message.delete()
-        await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been declined by our moderators.',
-                               reply_to_message_id=int(lst_msg_id))
+@Client.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text) & filters.private & filters.incoming)
+async def send_for_index(bot, message):
+    if message.text:
+        regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
+        match = regex.match(message.text)
+        if not match:
+            return await message.reply('Invalid link')
+        chat_id = match.group(4)
+        last_msg_id = int(match.group(5))
+        if chat_id.isnumeric():
+            chat_id = int(("-100" + chat_id))
+    elif message.forward_from_chat.type == enums.ChatType.CHANNEL:
+        last_msg_id = message.forward_from_message_id
+        chat_id = message.forward_from_chat.username or message.forward_from_chat.id
+    else:
         return
 
-    if lock.locked():
-        return await query.answer('Wait until previous process complete.', show_alert=True)
-
-    msg = query.message
-    await query.answer('Processing...⏳', show_alert=True)
-
-    if int(from_user) not in ADMINS:
-        await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
-                               reply_to_message_id=int(lst_msg_id))
-
-    await msg.edit(
-        "Starting Indexing",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
-        )
-    )
     try:
-        chat = int(chat)
+        await bot.get_chat(chat_id)
+    except ChannelInvalid:
+        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+    except (UsernameInvalid, UsernameNotModified):
+        return await message.reply('Invalid Link specified.')
+    except Exception as e:
+        logger.exception(e)
+        return await message.reply(f'Errors - {e}')
+
+    try:
+        k = await bot.get_messages(chat_id, last_msg_id)
     except:
-        chat = chat
-    await index_files_to_db(int(lst_msg_id), chat, msg, bot)
+        return await message.reply('Make sure that I am an admin in the channel, if the channel is private')
+    
+    if k.empty:
+        return await message.reply('This may be a group and I am not an admin of the group.')
+
+    # If the user is an admin, skip the moderation request and start indexing automatically
+    if message.from_user.id in ADMINS:
+        await message.reply(f"Auto-indexing started for Chat ID: <code>{chat_id}</code> and Last Message ID: <code>{last_msg_id}</code> since you're an admin.")
+        # Automatically proceed with indexing
+        await index_files_to_db(last_msg_id, chat_id, message, bot)
+    else:
+        # Non-admin users will go through the approval process
+        if type(chat_id) is int:
+            try:
+                link = (await bot.create_chat_invite_link(chat_id)).invite_link
+            except ChatAdminRequired:
+                return await message.reply('Make sure I am an admin in the chat and have permission to invite users.')
+        else:
+            link = f"@{message.forward_from_chat.username}"
+
+        buttons = [
+            [
+                InlineKeyboardButton('Accept Index',
+                                     callback_data=f'index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}')
+            ],
+            [
+                InlineKeyboardButton('Reject Index',
+                                     callback_data=f'index#reject#{chat_id}#{message.id}#{message.from_user.id}'),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+
+        await bot.send_message(LOG_CHANNEL,
+                               f'#IndexRequest\n\nBy : {message.from_user.mention} (<code>{message.from_user.id}</code>)\nChat ID/Username: <code>{chat_id}</code>\nLast Message ID: <code>{last_msg_id}</code>\nInvite Link: {link}',
+                               reply_markup=reply_markup)
+        await message.reply('Thank you for the contribution. Wait for my moderators to verify the files.')
+
 
 
 @Client.on_message((filters.forwarded | (filters.regex("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")) & filters.text) & filters.private & filters.incoming)
